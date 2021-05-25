059e2faab89221b261f3a5e1de35fda3f44ef16f
Siseum <62826105+Siseum@users.noreply.github.com>
2021-05-25 13:51:54 +0900

    Create 3진법 뒤집기.cpp

---
+++ 3진법 뒤집기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int N=n;
+    string str;
+    
+    while(N !=0)
+    {
+        str.push_back(N%3);
+        N=N/3;
+    }
+    
+    N=1;
+    for(int i=str.size()-1;i>=0;i--)
+    {
+        answer+=(str[i])*N;
+        N=N*3;
+    }
+    
+    
+    
+    return answer;
+}
