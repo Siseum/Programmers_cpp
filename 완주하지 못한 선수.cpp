2d9898e6ca32694327f0623402de420a98fcd29c
Siseum <62826105+Siseum@users.noreply.github.com>
2021-05-27 17:28:51 +0900

    Create 완주하지 못한 선수.cpp

---
+++ 완주하지 못한 선수.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+
+string solution(vector<string> participant, vector<string> completion) 
+{
+    string answer;
+    
+    map<string,int> Hash;
+    map<string,int> Hash2;
+    
+    int N1=participant.size();
+    int N2=completion.size();
+    
+    for(int i=0;i<N1;i++)
+        Hash[participant[i]]++;
+    
+    for(int i=0;i<N2;i++)
+        Hash2[completion[i]]++;
+    
+    for(int i=0;i<N1;i++)
+    {
+        if(Hash[participant[i]] != Hash2[participant[i]])
+        {
+            answer = participant[i];
+            break;
+        }
+    }
+       
+    return answer;
+}
