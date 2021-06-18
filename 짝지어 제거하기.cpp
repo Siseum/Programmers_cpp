9d17af14ada7179f3c8693f1ca51c4e3b0950d69
Siseum <62826105+Siseum@users.noreply.github.com>
2021-06-18 11:05:28 +0900

    Create 짝지어 제거하기.cpp

---
+++ 짝지어 제거하기.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    int answer = 0;
+    
+    if(s.size() % 2 == 1)
+        return answer;
+    
+    stack<char> st;
+    
+    for(int i=0;i<s.size();i++)
+    {
+        if(st.empty() || st.top() != s[i])
+        {
+            st.push(s[i]);
+            continue;
+        }
+        
+        st.pop();   
+    }
+    
+    if(st.empty())
+        answer = 1;
+    else
+        answer = 0;
+    
+    return answer;
+}
