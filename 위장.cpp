fe6677a246699891679e943b1970e57ad01bacb7
Siseum <62826105+Siseum@users.noreply.github.com>
2021-07-07 11:29:41 +0900

    Create 위장.cpp

---
+++ 위장.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes)
+{
+    int answer = 1;
+    unordered_map <string, int> info;
+    vector<string> name;
+
+    for (int i = 0; i < clothes.size(); i++)
+    {
+        string s = clothes[i][1];
+        if (info[s] == 0)
+            name.push_back(s);
+
+        info[s]++;
+    }
+    
+    for (int i = 0; i < name.size(); i++)
+        answer = answer * (info[name[i]] + 1);
+
+    return answer - 1;
+}
